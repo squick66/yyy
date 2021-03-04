81755ec5452b86616d45b7068ee554d8de3ef3a9
uzytkownik <eset@gmail.com>
2021-03-04 22:08:54 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] tab = {1,5,8,4,6,7};
+
+        for (int index: tab) {
+            System.out.print(index + " ");
+        }
+
+        int b;
+    }
+}
