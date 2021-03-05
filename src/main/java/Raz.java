8839ba85c06ba60f226936e03516457acc953d5b
uzytkownik <eset@gmail.com>
2021-03-06 00:04:41 +0100

    utworzenie getery settery klasy Raz

--- src/main/java/Raz.java
+++ src/main/java/Raz.java
@@ -2,8 +2,25 @@ public class Raz {
     String marka;
     int rocznik;
 
+
     public Raz(String marka, int rocznik) {
         this.marka = marka;
+        this.rocznik = rocznik;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public int getRocznik() {
+        return rocznik;
+    }
+
+    public void setRocznik(int rocznik) {
         this.rocznik = rocznik;
     }
 }
