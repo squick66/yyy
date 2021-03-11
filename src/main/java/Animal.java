742fdef68a8baf41af4a2a6e57f7be8502bb91ec
uzytkownik <eset@gmail.com>
2021-03-11 16:37:11 +0100

    Utworzenie metody abstrakcyjnej i klasy Pies.

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,4 +1,6 @@
-public class Animal {
+public abstract class Animal {
     String name;
     int age;
+
+    public abstract double getAge();
 }
---
+++ src/main/java/Pies.java
@@ -0,0 +1,11 @@
+public class Pies extends Animal {
+
+    public Pies(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    @Override
+    public double getAge() {
+        return age;
+    }
+}
