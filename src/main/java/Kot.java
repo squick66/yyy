d246c8559f06dcaa8a796dd19632d6fd52fd536b
uzytkownik <eset@gmail.com>
2021-03-11 16:47:35 +0100

    Utworzenie klasy Kot

---
+++ src/main/java/Kot.java
@@ -0,0 +1,13 @@
+public class Kot extends Animal {
+
+
+    @Override
+    public double getAge() {
+        return age;
+    }
+
+    @Override
+    public String getString() {
+        return name;
+    }
+}
