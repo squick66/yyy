1949dc38fae43dddae53d1571d7db0dbf0c23775
uzytkownik <eset@gmail.com>
2021-03-20 18:54:59 +0100

    Dodanie listy do listy.

--- src/main/java/lambda/set/Main.java
+++ src/main/java/lambda/set/Main.java
@@ -1,4 +1,16 @@
 package lambda.set;
 
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Set<String> names = new TreeSet<>(Set.of("Rafał", "Darek", "Franek", "Arek"));
+        System.out.println(names);
+        Set<String> namen = new HashSet<>(Set.of("Rafał", "Darek", "Franek", "Arek"));
+        System.out.println(namen);
+
+        Integer[] numerous = {1,2,8,9,1,8,9};
+        Set<Object> numb = new LinkedHashSet<Object>();
+        System.out.println(numb);
+    }
 }
